b6b77d42297ca66e660c68029e21a457d7329395
bookshiyi <14087838+bookshiyi@users.noreply.github.com>
2026-08-16 08:48:48 +0000

    chore: update studio to 0.1.0-ff8af8d

--- Casks/studio.rb
+++ Casks/studio.rb
@@ -1,9 +1,9 @@
 cask "studio" do
-  version ""
-  sha256 "73a28cd4074fa8073d20d611d42f89f027628306e35297b434fb2e2ee8fa630d"
+  version "0.1.0-ff8af8d"
+  sha256 "988bbb7018c8fb4593b9c3568d76395f5ba1b5e06db41c13b986f86627246780"
   bundle_id "com.insightop.studio"
 
-  url "https://vault.insightop.com/studio/darwin/Studio--arm64.dmg",
+  url "https://vault.insightop.com/studio/darwin/Studio-0.1.0-ff8af8d-arm64.dmg",
       verified: "vault.insightop.com/studio/darwin/"
   name "Studio"
   desc "Studio 桌面端（Electron 薄壳：多标签加载远程实例）"
