65e4e23f58be41e9f56486c65383f0cdc480a334
book诗意 <tech.gosling0d@icloud.com>
2026-09-21 12:08:13 +0800

    fix: 移除 pixso cask 已废弃的 verified 参数

--- Casks/pixso.rb
+++ Casks/pixso.rb
@@ -2,8 +2,7 @@ cask "pixso" do
   version "2.3.1"
   sha256 "d7429f7f608c9e8ebcc6bbb9477dd91c701a7418451bd40132516ed61e8b41bb"
 
-  url "https://pixso-pub-prod.obs.cn-east-3.myhuaweicloud.com/cms/download/package/app/bosyun/#{version}/Pixso_mac_v#{version.gsub(".", "_")}.dmg",
-      verified: "pixso-pub-prod.obs.cn-east-3.myhuaweicloud.com/cms/download/"
+  url "https://pixso-pub-prod.obs.cn-east-3.myhuaweicloud.com/cms/download/package/app/bosyun/#{version}/Pixso_mac_v#{version.gsub(".", "_")}.dmg"
   name "Pixso"
   desc "Collaborative UI design tool"
   homepage "https://pixso.cn/"
